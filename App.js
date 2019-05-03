e53253ac3b51c93dd58967615ea474da31be416c
Michael Stotzer <michael.stotzer@fhnw.ch>
2019-05-03 11:37:22 +0200

    Issue version number (#22)
    * issue version number 0.1
    
    * issue version number color #9c27b0
    
    * Takes the version number to be displayed from package.json

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import Programming from "./Components/Screens/Programming/Programming";
 import Settings from "./Components/Screens/Settings/Settings";
 import BleService from "./communication/BleService";
 import {set_update_device_name_callback, device_name} from "./Stores/SettingsStore";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import {getStatusBarHeight, ifIphoneX} from "react-native-iphone-x-helper";
+import { version } from './package.json';
 
 export default class App extends Component {
     state = {device: undefined};
@@ -30,25 +31,39 @@ class DrawerContent extends Component {
 
     render() {
         return (
-            <View>
-                <View
-                    style={{
-                        backgroundColor: '#9c27b0',
-                        height: 140,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        ...ifIphoneX({
-                            paddingTop: getStatusBarHeight() + 10,
-                        }, {
-
-                        })
-                    }}
-                >
-                    <Text style={{ color: 'white', fontSize: 30 }}>
-                        {this.state.device_name}
+            <View style={styles.container}>
+                <View style={styles.deviceName}>
+                    <Text
+                        style={{ color: 'white', fontSize: 30 }}>
+                            {this.state.device_name}
                     </Text>
                 </View>
-                <DrawerItems {...this.props} />
+                <View style ={styles.drawerItems}>
+                    <DrawerItems {...this.props} />
+                </View>
+                <View style={styles.footer}>
+                    <Text
+                        style={{
+                            flex: 1,
+                            marginLeft: 15,
+                            color: 'white',
+                            fontWeight: 'bold',
+                        }}
+                    >
+                        robby app
+                    </Text>
+                    <Text
+                        style={{
+                            flex: 1,
+                            textAlign: 'right',
+                            marginRight: 15,
+                            color: 'white',
+                            fontWeight: 'bold',
+                        }}
+                    >
+                        v{version}
+                    </Text>
+                </View>
             </View>
         );
     }
@@ -62,3 +77,32 @@ const DrawerNavigator = createDrawerNavigator({
 });
 
 const DrawerContainer = createAppContainer(DrawerNavigator);
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    deviceName: {
+        flex: 1,
+        backgroundColor: '#9c27b0',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        ...ifIphoneX({
+                paddingTop: getStatusBarHeight() + 10,
+            }, {}
+        )
+    },
+    drawerItems: {
+        flex: 3,
+        flexDirection: 'column',
+    },
+    footer: {
+        backgroundColor: '#9c27b0',
+        height: 50,
+        alignItems: 'center',
+        borderTopWidth: 1,
+        borderTopColor: '#9c27b0',
+        flexDirection: 'row',
+    },
+})
