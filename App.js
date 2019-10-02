c8986eee82bef3f98c1dce9320a74dcef41b5111
Robin Schoch <robin.schoch@fhnw.ch>
2019-10-02 13:47:26 +0200

    added ios support

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import Programming from './src/components/screens/programming/Programming';
 import Settings from './src/components/screens/settings/Settings';
 import BleService from './src/communication/BleService';
 import {
-    addDeviceNameChangeListener,
-    getDeviceName,
+  addDeviceNameChangeListener,
+  getDeviceName,
 } from './src/stores/SettingsStore';
 import {View, Text, StyleSheet} from 'react-native';
 import {getStatusBarHeight, ifIphoneX} from 'react-native-iphone-x-helper';
@@ -14,107 +14,107 @@ import {version} from './package.json';
 import i18n from './resources/locales/i18n';
 
 export default class App extends Component {
-    state = {device: undefined};
+  state = {device: undefined};
 
-    componentWillMount() {
-        BleService.requestLocationPermission();
-    }
+  componentWillMount() {
+    BleService.requestLocationPermission();
+  }
 
-    render() {
-        return <DrawerContainer/>;
-    }
+  render() {
+    return <DrawerContainer/>;
+  }
 }
 
 class DrawerContent extends Component {
-    state = {
-        device_name: getDeviceName(),
-    };
+  state = {
+    device_name: getDeviceName(),
+  };
 
-    constructor() {
-        super();
-        console.log('Welcome to robo ');
+  constructor() {
+    super();
+    console.log('Welcome to robo ');
 
-        addDeviceNameChangeListener(name => {
-            this.setState({device_name: name});
-        });
-    }
+    addDeviceNameChangeListener(name => {
+      this.setState({device_name: name});
+    });
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.deviceName}>
-                    <Text style={{color: 'white', fontSize: 30}}>
-                        {this.state.device_name}
-                    </Text>
-                </View>
-                <View style={styles.drawerItems}>
-                    <DrawerNavigatorItems {...this.props} />
-                </View>
-                <View style={styles.footer}>
-                    <Text
-                        style={{
-                            flex: 1,
-                            marginLeft: 15,
-                            color: 'white',
-                            fontWeight: 'bold',
-                        }}>
-                        robby app
-                    </Text>
-                    <Text
-                        style={{
-                            flex: 1,
-                            textAlign: 'right',
-                            marginRight: 15,
-                            color: 'white',
-                            fontWeight: 'bold',
-                        }}>
-                        v{version}
-                    </Text>
-                </View>
-            </View>
-        );
-    }
+  render() {
+    return (
+        <View style={styles.container}>
+          <View style={styles.deviceName}>
+            <Text style={{color: 'white', fontSize: 30}}>
+              {this.state.device_name}
+            </Text>
+          </View>
+          <View style={styles.drawerItems}>
+            <DrawerNavigatorItems {...this.props} />
+          </View>
+          <View style={styles.footer}>
+            <Text
+                style={{
+                  flex: 1,
+                  marginLeft: 15,
+                  color: 'white',
+                  fontWeight: 'bold',
+                }}>
+              robby app
+            </Text>
+            <Text
+                style={{
+                  flex: 1,
+                  textAlign: 'right',
+                  marginRight: 15,
+                  color: 'white',
+                  fontWeight: 'bold',
+                }}>
+              v{version}
+            </Text>
+          </View>
+        </View>
+    );
+  }
 }
 
 const DrawerNavigator = createDrawerNavigator(
     {
-        [i18n.t('App.programming')]: {screen: Programming},
-        [i18n.t('App.settings')]: {screen: Settings},
+      [i18n.t('App.programming')]: {screen: Programming},
+      [i18n.t('App.settings')]: {screen: Settings},
     },
     {
-        contentComponent: DrawerContent,
+      contentComponent: DrawerContent,
     },
 );
 
 const DrawerContainer = createAppContainer(DrawerNavigator);
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    deviceName: {
-        flex: 1,
-        backgroundColor: '#9c27b0',
-        height: 140,
-        alignItems: 'center',
-        justifyContent: 'center',
-        ...ifIphoneX(
-            {
-                paddingTop: getStatusBarHeight() + 10,
-            },
-            {},
-        ),
-    },
-    drawerItems: {
-        flex: 3,
-        flexDirection: 'column',
-    },
-    footer: {
-        backgroundColor: '#9c27b0',
-        height: 50,
-        alignItems: 'center',
-        borderTopWidth: 1,
-        borderTopColor: '#9c27b0',
-        flexDirection: 'row',
-    },
+  container: {
+    flex: 1,
+  },
+  deviceName: {
+    flex: 1,
+    backgroundColor: '#9c27b0',
+    height: 140,
+    alignItems: 'center',
+    justifyContent: 'center',
+    ...ifIphoneX(
+        {
+          paddingTop: getStatusBarHeight() + 10,
+        },
+        {},
+    ),
+  },
+  drawerItems: {
+    flex: 3,
+    flexDirection: 'column',
+  },
+  footer: {
+    backgroundColor: '#9c27b0',
+    height: 50,
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: '#9c27b0',
+    flexDirection: 'row',
+  },
 });
--- ios/File.swift
+++ ios/File.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  robby_app
+//  robbyapp60
 //
 //  Created by Robin Schoch on 02.10.19.
 //  Copyright © 2019 Facebook. All rights reserved.
