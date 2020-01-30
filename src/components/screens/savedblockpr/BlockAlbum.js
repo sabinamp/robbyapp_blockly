45be63cf8867be89c6003fdd0c7f205ba8bda7b9
Sabina Borbely <sabina.borbely@students.fhnw.ch>
2020-01-30 15:50:58 +0100

    changed the menu position

--- src/components/screens/savedblockpr/BlockAlbum.js
+++ src/components/screens/savedblockpr/BlockAlbum.js
@@ -31,7 +31,6 @@ class BlockAlbum extends Component {
 
   state = {
     dataSource: [],
-    modalVisible: false,
     isRefreshing: false,
   }
 
@@ -67,45 +66,30 @@ class BlockAlbum extends Component {
     this.onRefresh();
   }
 
-  openModal() {
-    this.setState({ modalVisible: true });
-  }
-
-  closeModal() {
-    this.setState({ modalVisible: false });
-  }
-
-
   onRefresh() {
     let data = this.props.blocks;
-    this.setState({ isRefreshing: true, dataSource: data }); // true isRefreshing flag for enable pull to refresh indicator    
-
+    this.setState({ isRefreshing: true, dataSource: data }); // true isRefreshing flag for enable pull to refresh    
     this.setState({ isRefreshing: false });//flag to disable pull to refresh indicator
+  }
+
+  openBlockly(item) {
+    //TODO
+
   }
 
   renderItem(item) {
     return (
       <View style={{ flex: 1, flexDirection: 'column', alignItems: 'stretch', margin: 4 }}>
 
-        <Menu style={{ alignSelf: 'flex-end', fontsize: '20', color: '#9C27B0' }}>
+        <Menu style={{ alignSelf: 'flex-start', fontsize: '20', color: '#9C27B0' }}>
           <MenuTrigger text='Select action:' />
           <MenuOptions>
-            <MenuOption text='Open' onSelect={() => this.openModal()} />
+            <MenuOption text='Open' onSelect={() => this.openBlockly(item)} />
             <MenuOption text='Delete' onSelect={() => this.onDeleteItem(item.block_name)} />
           </MenuOptions>
         </Menu>
-        <Button style={{ alignSelf: 'flex-start' }} blockname={item.block_name} colorHolder={getRandomColor()} />
+        <Button style={{ alignSelf: 'stretch' }} blockname={item.block_name} colorHolder={getRandomColor()} />
 
-
-        <Modal visible={this.state.modalVisible} style={{ flex: 1, justifyContent: 'center' }}
-          animationType={'slide'}
-          onRequestClose={() => this.closeModal()} >
-          <Blockly block={item} />
-          <Button
-            onPress={() => this.props.navigation.goBack()}
-            title="Dismiss"
-          />
-        </Modal>
       </View>
 
     );
@@ -128,16 +112,15 @@ class BlockAlbum extends Component {
             //Setting the number of columns
             numColumns={1}
           />
-
         </SafeAreaView>
       </MenuProvider>
     );
   }
+
 }
 
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     padding: 20,
